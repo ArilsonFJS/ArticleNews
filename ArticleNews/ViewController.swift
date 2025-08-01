a5daa31887dae90b5d7411e79439a29aedb1399a
ArilsonFJS <arilsonsilva61@gmail.com>
2025-08-01 16:50:59 -0300

    Iniciando projeto

---
+++ ArticleNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleNews
+//
+//  Created by Arilson Silva on 01/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
